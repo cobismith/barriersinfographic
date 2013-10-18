02ac6597cb6451b75abf0940072538066360d326
cobismith <cobi.smith@gmail.com>
2013-10-18 13:59:52 +0700

    Update recycle.js

--- recycle.js
+++ recycle.js
@@ -1,6 +1,6 @@
 /**
  * Our rel closure contains functions to show the relationships
- * between actors and the shows they're in.  It also handles the
+ * between countries and the barriers indigneous people face there. Note terms still refer to actors/shows from the HBOrecycling project the code originated from in GitHub.  It also handles the
  * drawing of all of our objects.  
  */
 var rel = {
